791b596bf7d3cdd980c5318baff7d70e383d8a09
Nick <nickfoden@gmail.com>
2017-03-22 16:30:37 -0400

    Now only one random result is displayed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var Edamam_URL = 'https://api.edamam.com/search';
-
+var random = '4';
 
 function getDataFromApi(searchTerm, callback) {
   var settings = {
@@ -8,7 +8,6 @@ function getDataFromApi(searchTerm, callback) {
       app_id: "b0c0dab4",
       app_key: "f312cd5d8d6125c74991aaca9988db77",
       q: searchTerm, 
-
     },
     dataType: 'json',
     type: 'GET',
@@ -17,21 +16,35 @@ function getDataFromApi(searchTerm, callback) {
   $.ajax(settings);
 }
 
-
-function displaySearchData(data) {
+function displaySearchData(data){
+  var resultArray = [];
   var resultElement = '';
   if (data.hits) {
-    data.hits.forEach(function(item) {
-      resultElement += `<a href="${item.recipe.url}" target="_blank">` + item.recipe.label + `</a>`,
-      resultElement += `<img src="${item.recipe.image}">` 
-    });
-  }
-  else {
-    resultElement += '<h4>No results</h4>';
-  }
-  
+      data.hits.forEach(function(item){
+        var resultObject = {};
+        resultObject.label = item.recipe.label,
+        resultObject.url = item.recipe.url,
+        resultObject.image = item.recipe.image
+             
+        resultArray.push(resultObject);
+      });
+      
+      var random = Math.floor((Math.random()*10)+1);
+      var finish = resultArray[random];
+      
+      console.log(finish);
+      
+      function display(finishData){
+        resultElement += `<a href="${finishData.url}" target="_blank">` + finishData.label + `</a><br>`,
+        resultElement += `<img src="${finishData.image}"><br>`
+      }
+      display(finish);
+      
+   }  else{
+      resultElement += `<h4>` + "No results" + `</h4>`;
+    }
   $('.js-search-results').html(resultElement);
-}
+};
 
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
