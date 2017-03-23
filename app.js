a57d97b65ede1734b6ce98916f832cc38bbc9606
Nick Foden <nickfoden@gmail.com>
2017-03-22 23:50:27 -0400

    Cleaned up indentation issues

--- app.js
+++ app.js
@@ -26,31 +26,31 @@ function getDataFromApi(searchTerm, callback) {
   $.ajax(settings);
 }
 
-function displaySearchData(data){
+function displaySearchData(data) {
   var resultArray = [];
   var resultElement = '';
   if (data.hits.length > 0) {
-      data.hits.forEach(function(item){
-        var resultObject = {};
-        resultObject.label = item.recipe.label,
-        resultObject.url = item.recipe.url,
-        resultObject.image = item.recipe.image
-             
-        resultArray.push(resultObject);
-      });
-      
-      var random = Math.floor((Math.random()*10)+1);
-      var finish = resultArray[random];
-      
-      function display(finishData){
-        resultElement += `<a href="${finishData.url}" target="_blank">` + finishData.label + `</a><br>`,
-        resultElement += `<a href="${finishData.url}" target="_blank"><img src="${finishData.image}"></a><br>`
-      }
-      display(finish);
-      
-   }  else{
-      resultElement += `<h4>` + "No results" + `</h4>`;
+    data.hits.forEach(function(item) {
+      var resultObject = {};
+      resultObject.label = item.recipe.label;
+      resultObject.url = item.recipe.url;
+      resultObject.image = item.recipe.image;
+           
+      resultArray.push(resultObject);
+    });
+    
+    var random = Math.floor((Math.random()*10)+1);
+    var finish = resultArray[random];
+    
+    function display(finishData) {
+      resultElement += `<a href="${finishData.url}" target="_blank">` + finishData.label + `</a><br>`,
+      resultElement += `<a href="${finishData.url}" target="_blank"><img src="${finishData.image}"></a><br>`
     }
+    display(finish);
+    
+  } else {
+    resultElement += `<h4>No results</h4>`;
+  }
   $('.js-search-results').html(resultElement);
 };
 
